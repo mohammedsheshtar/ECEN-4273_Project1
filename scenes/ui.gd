5748a47017cf18b7dc6501d1254da8486ef16041
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2024-10-21 07:39:26 -0500

    player UI
    added health bar and timer on screen

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,1 +1,20 @@
 extends CanvasLayer
+
+@onready var player = get_tree().get_first_node_in_group('player')
+@onready var timer_label = $TimerLabel  # Reference the label directly from CanvasLayer
+
+var time_left := 90.0  # Use a float for precision
+
+func _ready():
+	set_process(true)  # Enable processing
+
+func _process(delta):
+	$MarginContainer/ProgressBar.value = player.health
+	update_timer(delta)
+
+func update_timer(delta):
+	if time_left > 0:
+		time_left -= delta
+		var minutes = int(time_left) / 60
+		var seconds = int(time_left) % 60
+		timer_label.text = str(minutes).pad_zeros(2) + ":" + str(seconds).pad_zeros(2)
